9afd8db5b077060154d3c2b4df61327f5db5a3fe
YeemiRouth <yeemirouth@gmail.com>
2023-07-25 18:55:32 +1000

    the most useless service possible

---
+++ JsonService/JsonService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web.Script.Serialization;
+
+/// <summary>
+/// JsonService
+/// </summary>
+public class JsonService
+{
+    /// <summary>
+    /// Convert json string to T
+    /// </summary>
+    /// <typeparam name="T">JSON Root</typeparam>
+    /// <param name="json">JSON string</param>
+    /// <returns>Finished JSON Root Instance</returns>
+    public static T GetT<T>(string json)
+    {
+        JavaScriptSerializer jss = new JavaScriptSerializer();
+
+        return jss.Deserialize<T>(json);
+    }
+
+    /// <summary>
+    /// Convert object to json string
+    /// </summary>
+    /// <param name="obj">JSON object</param>
+    /// <returns>Finished JSON string</returns>
+    public static string FromObj(object obj)
+    {
+        JavaScriptSerializer jss = new JavaScriptSerializer();
+
+        return jss.Serialize(obj);
+    }
+}
---
+++ JsonService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace JsonServiceUtil
+{
+    class TestRoot
+    {
+        public TestRoot() {} // required for json deserialization
+
+        public TestRoot(string ello)
+        {
+            this.ello = ello;
+        }
+
+        public string ello { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestRoot testRoot = new TestRoot("test1");
+
+            Console.WriteLine(JsonService.FromObj(testRoot)); // {"ello":"test1"}
+
+            TestRoot testRoot2 = JsonService.GetT<TestRoot>("{\"ello\":\"test2\"}");
+
+            Console.WriteLine(testRoot2.ello); // test2
+
+            Console.ReadKey();
+        }
+    }
+}
